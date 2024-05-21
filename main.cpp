ce78471da973639b86ca8289045872dbc48cf923
brastlia <120080062+brastli@users.noreply.github.com>
2024-05-21 18:07:32 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// Name: Sofia Kolobaev, Page Patterson, Minghao Zhu
+// Group ID: kolpatzhu
+// Group Number: 6
+// Problem Set: Project 1
+// Main program file for project 1
+
+#include <iostream>
+#include <vector>
+#include "Code.h"
+
+using namespace std;
+
+int main()
+{
+    Mastermind game;
+    game.playGame();
+    return 0;
+}
+
